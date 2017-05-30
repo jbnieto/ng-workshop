a69b1963c41e24c2fc566880b2dba3ea22cebe95
Jose Julian Betancur Nieto <jose.betancur@globant.com>
2017-05-29 21:01:06 -0500

    Adding cookie OAuth Spotify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,19 @@
 export class AppConfig {
-    public readonly apiUrl = 'https://api.spotify.com/v1/search';
+    public readonly apiUrl = 'https://api.spotify.com/v1/';
+    public readonly token = this.getCookie('token');
+
+    private getCookie(name: string) {
+        let ca: Array<string> = document.cookie.split(';');
+        let caLen: number = ca.length;
+        let cookieName = `${name}=`;
+        let c: string;
+
+        for (let i: number = 0; i < caLen; i += 1) {
+            c = ca[i].replace(/^\s+/g, '');
+            if (c.indexOf(cookieName) == 0) {
+                return c.substring(cookieName.length, c.length);
+            }
+        }
+        return '';
+    }
 };
