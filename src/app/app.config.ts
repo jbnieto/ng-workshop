4fd15dcd4ca37d1521ec3099824e95a3a9daed81
Jose Julian Betancur Nieto <jose.betancur@globant.com>
2017-06-01 11:02:51 -0500

    adding static token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 export class AppConfig {
     public readonly apiUrl = 'https://api.spotify.com/v1/';
-    public readonly token = this.getCookie('token');
+    //public readonly token = this.getCookie('token');
+
+    public readonly token = "BQAM7FlfxNK4aNDAiie9TA40eW7fGWBEFj1MJxM4ZvfHoEms980UaBxfsl-DAitmGU-NmxLXT3dP3BuwOmr6F987qG5bzHVBw9ur2t-Fa7nuPCLCqhYqooDMyTWiCDbfDzVf6cVtQvd8";
 
     private getCookie(name: string) {
         let ca: Array<string> = document.cookie.split(';');
